30092c01d816d520b3d8a8087394a22247e06702
Mateus Barbosa <mbmachado@outlook.com>
2019-09-29 09:17:32 -0300

    adds initial files

---
+++ symbol.h
@@ -0,0 +1,18 @@
+#include <utility>
+#include <vector>
+
+
+vector< pair<string, string> > TOKENS = {
+	{"PROGRAMAINICIO", },
+	{"EXECUCAOINICIO", "execucaoinicio"},
+	{"fimexecucao", "fimexecucao"},
+	{"fimprograma", "fimprograma"},
+	{"definainstrucao"}
+	{"como"}
+	{"inicio"}
+	{"fim"}
+	{"repita"}
+	{"vezes"}
+	{"repita"}
+};
+
